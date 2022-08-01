cd1d647f9af2d31f71eb5fd9d72e5250b135c013
Sean Lynch <slynch1223@gmail.com>
2022-08-01 14:59:14 -0400

    Create main.tf

---
+++ s3_bucket/main.tf
@@ -0,0 +1,41 @@
+#tfsec:ignore:aws-s3-enable-bucket-logging
+resource "aws_s3_bucket" "this" {
+  #checkov:skip=CKV_AWS_144:Not needed for testing
+  #checkov:skip=CKV_AWS_18:Not needed for testing
+  bucket_prefix = "slynch1223-tf-"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
