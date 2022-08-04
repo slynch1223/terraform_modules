960de1f21cb54ecfb84f32a398a7f8aaa48cc2f4
Sean Lynch <slynch1223@gmail.com>
2022-08-04 18:11:02 -0400

    update VPC CIDR validation

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -3,7 +3,7 @@ variable "cidr_block" {
   type        = string
 
   validation {
-    condition     = can(regex("^[0-9.]{3,63}$", var.cidr_block))
-    error_message = "Must be a valid S3 cidr_block (^[0-9.]{3,63}$)"
+    condition     = can(regex("^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-9]))?$", var.cidr_block))
+    error_message = "Must be a valid S3 cidr_block (^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-9]))?$)"
   }
 }
