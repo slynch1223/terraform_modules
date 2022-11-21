ad99e85d33bfdbfa621a51690e16b39d98bfd3ba
Sean Lynch <slynch1223@gmail.com>
2022-11-21 11:12:32 -0500

    updated vpc module

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,5 +1,5 @@
-# output "arn" { value = aws_s3_bucket.this.arn }
-# output "domain_name" { value = aws_s3_bucket.this.bucket_domain_name }
-# output "hosted_zone_id" { value = aws_s3_bucket.this.hosted_zone_id }
-# output "name" { value = aws_s3_bucket.this.id }
-# output "regional_domain_name" { value = aws_s3_bucket.this.bucket_regional_domain_name }
+output "arn" { value = aws_vpc.this.arn }
+output "id" { value = aws_vpc.this.id }
+output "nacl_id" { value = aws_vpc.this.default_network_acl_id }
+output "route_table_id" { value = aws_vpc.this.default_route_table_id }
+output "security_group_id" { value = aws_vpc.this.default_security_group_id }
